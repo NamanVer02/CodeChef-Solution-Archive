4832ae16dac5d3b28ff9ed12915889fdc1742531
Naman Verma <82778097+NamanVer02@users.noreply.github.com>
2022-03-20 17:19:04 +0530

    Create NOTEBOOK.c

---
+++ 0-1000 C/NOTEBOOK.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int T, X;
+    scanf("%d", &T);
+    for (int i = 0; i < T; i++){
+        scanf("%d", &X);
+        printf("%d\n", X*10);
+    }
+    return 0;
+}
