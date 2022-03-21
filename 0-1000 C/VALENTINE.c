31d5514a68cdede16e40f3577bd9be17e552c8dc
Naman Verma <82778097+NamanVer02@users.noreply.github.com>
2022-03-21 10:32:01 +0530

    Create VALENTINE.c

---
+++ 0-1000 C/VALENTINE.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int T, X, Y;
+    scanf("%d", &T);
+    for (int i = 0; i < T; i++){
+        scanf("%d %d", &X, &Y);
+        printf("%d\n", X / Y);
+    }
+    return 0;
+}
