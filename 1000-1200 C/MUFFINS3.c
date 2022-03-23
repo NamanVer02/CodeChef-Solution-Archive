9ed73917f4d3e7c9d939309fe4c111b0ce7154bd
Naman Verma <82778097+NamanVer02@users.noreply.github.com>
2022-03-23 14:46:00 +0530

    Create MUFFINS3.c

---
+++ 1000-1200 C/MUFFINS3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int T, N;
+    scanf("%d", &T);
+    for (int i = 0; i < T; i++){
+        scanf("%d", &N);
+        printf("%d\n", N/2 + 1);
+    }
+    return 0;
+}
